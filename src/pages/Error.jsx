f7cf54713b942609f7daea88f80a57c553117477
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-02-16 15:05:48 +0100

    Review error style

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,15 +5,13 @@ import '../utils/style/Error.css';
 function Error() {
     useEffect(() => { document.title = "HRnet - 404" })
     return (
-        <div className="bg-dark">
-            <section className="section-error">
-                <h1>404</h1>
-                <p>
-                    Oups! La page que vous demandez n’existe pas.
-                </p>
-                <Link to='/'>Retourner à la page d'accueil</Link>
-            </section>
-        </div>
+        <section className="section-error">
+            <h2>404</h2>
+            <p>
+                Oups! La page que vous demandez n’existe pas.
+            </p>
+            <Link to='/'>Retourner à la page d'accueil</Link>
+        </section>
     )
 }
 
